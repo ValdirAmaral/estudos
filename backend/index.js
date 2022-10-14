3e4275ffce37b62b308974df3952b6a65796575c
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2022-10-14 13:48:31 -0300

    add cors
    isntalado o npm cors, criado a constante de importação do cors e criado o mmiddleware

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const app = express()
 const port = process.env.PORT || 4040 
 const path = require("path")
 const userRout = require('./rotas')
+const cors = require('cors')
 
 const basePath = path.join(__dirname, 'templates')
 
+app.use(cors())
 app.use('/rotas', userRout)
 
 
