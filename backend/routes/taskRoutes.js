3621efd795ad587d9e4fda706f5a819df7735232
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2022-10-18 17:05:11 -0300

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const TaskController = require('../controllers/taskController.js')
+const TaskController = require('../controllers/TaskController.js')
 const Task = require('../models/Task.js')
 
 router.get('/hello', TaskController.showTasks)
