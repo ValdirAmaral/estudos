04d41396f91ebb7dce8db0ae67b693fb7334bbb2
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2022-10-23 11:37:22 -0300

    Add dotenv
    Adicionei o dotenv para a variável de ambiente impedindo o acesso aos dados do banco.

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,11 +1,27 @@
 
 const { Sequelize } = require('sequelize')
+const dotenv = require('dotenv').config()
+
+const dbName = process.env.DB_NAME
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
+
+
+const sequelize = new Sequelize (dbName, dbUser, dbPassword, {
+    dialect: 'mysql',
+    host: dbHost,
+    port: dbPort,
+})
+
+/*
 const sequelize = new Sequelize('tasks', 'root', '1234trocar', {
     host: 'tcp-mo2.mogenius.io',
     port:'10720',
     dialect: 'mysql'
 })
-
+*/
 try {
 
     sequelize.authenticate()
