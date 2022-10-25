ae8e16c8f6fb1d8af55949d6412b95120bebfb64
Daniel Porto <39194683+dankobaia@users.noreply.github.com>
2022-10-25 17:13:18 -0300

    review do userController

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,8 @@
 const userService = require("../services/users/userService");
+//Review: evita chamar a conexão com o banco aqui no controller, sempre tenta chamar os services pra dividir melhor os conceitos
 const User = require("../models/Users");
+
+
 const bcrypt = require("bcrypt");
 
 //exportar as classes das rotas
@@ -10,7 +13,13 @@ module.exports = class UserController {
 
     //checagem de email existente
     const { email } = req.body;
+    
+    //Review:essa parte deveria estar dentro do userService
+    //deveria ter uma função lá tipo function isUserExists(email)
+    // ela retornaria true ou false    
     const emailCheck = await User.findOne({ where: { email: email }});
+    
+    
     if (emailCheck) {
 
       res.json({
@@ -25,7 +34,12 @@ module.exports = class UserController {
     var dados = req.body;
 
     dados.password = await bcrypt.hash(dados.password, 8);
-
+    
+    
+    //Review: essa parte tb deveria estar dentro do userService,
+    //uma função como function createUser(userData)
+    // tb poderia retorna true ou false se desse certo ou errado
+    // e aqui no controller vc faria o if pra responder o json se deu certo ou não
     await User.create(dados)
       .then(() => {
         return res.json({
@@ -57,6 +71,7 @@ module.exports = class UserController {
   }
 
   static async showUsers(req, res) {
+    //Review: acho que vc montou como teste mas essa parte tb deveria estar dentro do userService
     const users = await User.findAll({
       attributes: ["username", "name", "email"],
     });
