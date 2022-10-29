45395d83dc17e110e204f19cd7e41ff1c4191268
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2022-10-28 23:25:58 -0300

    att readme

--- backend/README.md
+++ backend/README.md
@@ -10,6 +10,7 @@ Rotas:
 /user/all = Mostra todos os users no banco
 /user/add = Cadastrar usuário
 /user/login = Logar usuário
+/user/me = Utilizando o token no bearer responde o ID do usuário que veio do login
 
 
 /hello = retorna um json simples
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
 const TaskController = require('../controllers/TaskController.js')
-const User = require('../models/Users.js')
+
 
 
 router.get('/hello', TaskController.showTasks)
